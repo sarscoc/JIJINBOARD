c3b3880935775db8bdb5b8414ae3f336056b48b9
sars <yam.dsp.azusa0624@gmail.com>
2026-08-30 13:17:44 +0900

    Refresh Spreadsheet comment marker layering assets

--- public/spreadsheet/sheet-comments.js
+++ public/spreadsheet/sheet-comments.js
@@ -1,4 +1,4 @@
 (()=>{
-  const v='20260830-7';
+  const v='20260830-8';
   document.write(`<script src="decouple-project-people.js?v=${v}"><\/script><link rel="stylesheet" href="sheet-comments-unified.css?v=${v}"><script src="sheet-comments-base.js?v=${v}"><\/script><script src="sheet-comments-controller.js?v=${v}"><\/script>`);
 })();
